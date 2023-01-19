7399f1b49e5fa5c7fff1ccfc84c2af45aac5edfa
SVAYounii <younes048@gmail.com>
2023-01-19 20:18:02 +0100

    USer changed

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -100,7 +100,7 @@ namespace S3_Api_indi.Controllers
 
         // Post: api/Users/5
         [HttpPost("LoginUser/")]
-        public async Task<ActionResult<UserWithToken>> LoginUser([FromBody] User user)
+        public async Task<ActionResult<User>> LoginUser([FromBody] User user)
         {
             user = await _context.Users
                                             .Where(u => u.Username == user.Username && u.Password == user.Password)
@@ -131,7 +131,7 @@ namespace S3_Api_indi.Controllers
 
         // POST: api/Users
         [HttpPost("Login")]
-        public async Task<ActionResult<UserWithToken>> Login([FromBody] User user)
+        public async Task<ActionResult<User>> Login([FromBody] User user)
         {
             user = await _context.Users
                                         .Where(u => u.Username == user.Username
